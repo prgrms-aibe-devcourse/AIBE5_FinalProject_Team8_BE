4213ac806fb7188b14bddf47176e4f400111459c
gyu98-mun <ansrb537@gmail.com>
2026-05-26 14:26:03 +0900

    test: RootinApplicationTests - 컨텍스트 로드 테스트

---
+++ src/test/java/com/Rootin/RootinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rootin;
+
+import com.Rootin.annotation.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+class RootinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
