4fa2dd588a1932cea2d682e9674cf7e0912ecda8
gyu98-mun <ansrb537@gmail.com>
2026-05-26 14:24:58 +0900

    feat: PostRepository 추가

---
+++ src/main/java/com/Rootin/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.Rootin.repository;
+
+import com.Rootin.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
