095338f614e61394a6cd6c3692f0163ea8e7e226
gyu98-mun <ansrb537@gmail.com>
2026-05-26 14:24:50 +0900

    feat: AiResultRepository 추가

---
+++ src/main/java/com/Rootin/repository/AiResultRepository.java
@@ -0,0 +1,7 @@
+package com.Rootin.repository;
+
+import com.Rootin.domain.AiResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AiResultRepository extends JpaRepository<AiResult, Long> {
+}
