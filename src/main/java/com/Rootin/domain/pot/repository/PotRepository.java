b5f50447ac1c7ca1e7a0b99972190d0dc8c37abd
최윤석 <aa01066303541@gmail.com>
2026-05-27 11:39:37 +0900

    fix: resolve pot domain conflict

--- src/main/java/com/Rootin/domain/pot/repository/PotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Rootin.domain.pot.repository;
-
-import com.Rootin.domain.pot.entity.Pot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PotRepository extends JpaRepository<Pot, Long> {
-}
