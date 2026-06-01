4c0a07c13b212728fee8d62ed784e61beb66cfe7
gyu98-mun <ansrb537@gmail.com>
2026-06-01 11:01:55 +0900

    feat(UserMeResponse.java): 프로필 수정 응답을 위한 bio 필드 추가 및 of() 매핑 반영

--- src/main/java/com/Rootin/domain/user/dto/UserMeResponse.java
+++ src/main/java/com/Rootin/domain/user/dto/UserMeResponse.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 // 로그인한 유저 정보 응답 DTO
-// API: [GET] /api/v1/users/me
+// API: [GET] /api/v1/users/me, [PATCH] /api/v1/users/me
 @Getter
 @Builder
 public class UserMeResponse {
@@ -14,6 +14,7 @@ public class UserMeResponse {
     private String email;
     private String nickname;
     private String profileImage;
+    private String bio;
     private int point;
 
     public static UserMeResponse of(User user) {
@@ -22,6 +23,7 @@ public class UserMeResponse {
                 .email(user.getEmail())
                 .nickname(user.getNickname())
                 .profileImage(user.getProfileImage())
+                .bio(user.getBio())
                 .point(user.getPoint())
                 .build();
     }
