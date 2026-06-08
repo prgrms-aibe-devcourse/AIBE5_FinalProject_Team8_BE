e362c604c0162335c76c0147d94bc68bc40028f9
최윤석 <aa01066303541@gmail.com>
2026-06-08 15:24:47 +0900

    refactor: 화분 요청 검증 상수 공통화

---
+++ src/main/java/com/Rootin/domain/garden/dto/PotRequestValidation.java
@@ -0,0 +1,10 @@
+package com.Rootin.domain.garden.dto;
+
+final class PotRequestValidation {
+
+    static final int TITLE_MAX_LENGTH = 10;
+    static final int DESCRIPTION_MAX_LENGTH = 25;
+
+    private PotRequestValidation() {
+    }
+}
--- src/main/java/com/Rootin/domain/garden/dto/PotUpdateRequest.java
+++ src/main/java/com/Rootin/domain/garden/dto/PotUpdateRequest.java
@@ -9,13 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PotUpdateRequest {
 
-    private static final int TITLE_MAX_LENGTH = 10;
-    private static final int DESCRIPTION_MAX_LENGTH = 25;
-
     @NotBlank(message = "화분 제목은 필수 입력 항목입니다.")
-    @Size(max = TITLE_MAX_LENGTH, message = "화분 제목은 최대 10자까지 입력 가능합니다.")
+    @Size(max = PotRequestValidation.TITLE_MAX_LENGTH, message = "화분 제목은 최대 {max}자까지 입력 가능합니다.")
     private String title;
 
-    @Size(max = DESCRIPTION_MAX_LENGTH, message = "화분 소개글은 최대 25자까지 입력 가능합니다.")
+    @Size(max = PotRequestValidation.DESCRIPTION_MAX_LENGTH, message = "화분 소개글은 최대 {max}자까지 입력 가능합니다.")
     private String description;
 }
