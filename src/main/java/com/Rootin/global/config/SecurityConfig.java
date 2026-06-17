5913b52b12874f9404fd0fafcdce3aba35c6d99f
kws0315 <kjk10191@gmail.com>
2026-06-17 14:40:24 +0900

    feat: CD 자동화 수정, /actuator/** 허용범위 증가

--- src/main/java/com/Rootin/global/config/SecurityConfig.java
+++ src/main/java/com/Rootin/global/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/test", "/h2-console/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/actuator/health", "/actuator/health/**", "/actuator/prometheus").permitAll() // Test/매트릭 수집도구들
+                        .requestMatchers("/actuator/**").permitAll() // Test/매트릭 수집도구들
 
                         // 사용자 인증을 발급해주는 API 경로
                         .requestMatchers(
